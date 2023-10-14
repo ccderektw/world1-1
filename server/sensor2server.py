e642e1147cb706476e0df937654369b3e5f27660
derekkuo <ccderektw@gmail.com>
2023-10-14 14:27:10 +0800

    modify sensor file

--- server/sensor2server.py
+++ server/sensor2server.py
@@ -1,5 +1,13 @@
 import requests
+import sys
 
-url = "http://192.168.3.111:5000/post_data"
-obj = {'id': '2', 'val': '0'}
+# init connection
+host = sys.argv[1]
+targets = sys.argv[2]
+url = host + "/post_data"
+
+# init board
+
+# main loop
+obj = {'id': str(targets), 'val': '0'}
 requests.post(url, data = obj)
