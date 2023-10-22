3ce2cd0941fc03086df2114adcd7f3ca5f0b0506
ccderek <ccderek1212@gmail.com>
2023-10-22 15:05:19 +0800

    sensor to lamp: done! ccderek@

--- server/sensor2server.py
+++ server/sensor2server.py
@@ -1,3 +1,5 @@
+import RPi.GPIO as GPIO
+import time
 import requests
 import sys
 
@@ -5,9 +7,23 @@ import sys
 host = sys.argv[1]
 targets = sys.argv[2]
 url = host + "/post_data"
+lamp_list = []
+status = 0
 
 # init board
+sensor_pin = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor_pin, GPIO.IN)
 
 # main loop
-obj = {'id': str(targets), 'val': '0'}
-requests.post(url, data = obj)
+while(1):
+    data = GPIO.input(sensor_pin)
+    if(data != status):
+        print(data)
+        status = data
+        obj = {'id': targets, 'val': int(status)}
+        requests.post(url, data = obj)
+        time.sleep(0.5) 
+        #post
+    #obj = {'id': '2', 'val': '0'}
+    #print(GPIO.input(sensor_pin))
